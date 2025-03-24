0963ef8494ad224570f743754a5b3c3094395be1
khoakhongwibuuu <anhkhoa.vv@gmail.com>
2025-03-24 21:56:00 +0700

    Wall Maria

--- Week02/wall.cpp
+++ Week02/wall.cpp
@@ -14,32 +14,33 @@ private:
 
 	bool reach(long long H, const vector<long long> &a, const vector<long long> &b, vector<pair<int, int>> &plan)
 	{
-
 		plan.clear();
-
 		int pos = 0;
 		for (int i = 0; i < a.size(); i++)
 		{
 			if (a[i] >= H)
 				continue;
-
 			while (pos < b.size() && b[pos] < H - a[i])
 				pos++;
-
 			if (pos >= b.size())
 				return 0;
-
 			plan.push_back({i, pos});
 			pos++;
 		}
-
 		return 1;
 	}
 
 public:
-	test() { this->max_height = 0; }
+	test()
+	{
+		this->max_height = 0;
+		cout << "Constructing testcase: " << this << endl;
+	}
 
-	~test() {}
+	~test()
+	{
+		cout << "Destructing testcase: " << this << endl;
+	}
 
 	void input()
 	{
@@ -50,7 +51,6 @@ public:
 			cin >> x;
 			a.push_back(x);
 		}
-
 		cin >> m;
 		for (int i = 0; i < m; i++)
 		{
@@ -65,7 +65,6 @@ public:
 		long long low = *min_element(a.begin(), a.end());
 		long long high = *max_element(a.begin(), a.end()) + *max_element(b.begin(), b.end());
 		max_height = low;
-
 		while (low <= high)
 		{
 			long long mid = (low + high) >> 1;
@@ -90,8 +89,10 @@ public:
 
 int main()
 {
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
 	int tc = 1;
-	// cin >> tc;
+	cin >> tc;
 	while (tc--)
 	{
 		test *testcase = new test();
